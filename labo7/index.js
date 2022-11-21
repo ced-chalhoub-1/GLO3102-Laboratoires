37e17230bfa05e70c0ec63a2056b53ee2a7375ad
Cédric Chalhoub <cedric.chalhoub.1@ulaval.ca>
2022-11-20 20:13:30 -0500

    Remise finale

--- labo7/index.js
+++ labo7/index.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const taskService = require("./services/tasks.js");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
-app.post("/users", taskService.createUser);
+const corsOptions = {
+    origin: "*",
+    methods: "*"
+};
 
-app.get("/:userid/tasks", taskService.getTasks);
-app.post("/:userid/tasks", taskService.createTask);
-app.put("/:userid/tasks/:id", taskService.editTask)
-app.delete("/:userid/tasks/:id", taskService.deleteTask);
+app.get("/:userid/tasks", cors(corsOptions), taskService.getTasks);
+app.post("/:userid/tasks", cors(corsOptions), taskService.createTask);
+app.put("/:userid/tasks/:id", cors(corsOptions), taskService.editTask)
+app.delete("/:userid/tasks/:id", cors(corsOptions), taskService.deleteTask);
 
 app.listen(8100);
 
--- labo7/services/tasks.js
+++ labo7/services/tasks.js
@@ -2,38 +2,55 @@ const uuid = require('uuid');
 
 const users = []
 
-exports.createUser = (req, res) => {
-    const userid = uuid.v4()
-    const user = {id: userid, tasks: []}
-    users.push(user);
-    res.send({id: userid});
-}
 
 exports.getTasks = (req, res) => {
     const userid = req.params.userid;
+    console.log(userid)
+    const user = {id: userid, tasks: []}
+    users.push(user);
+    console.log(users);
+    if(!userid || userid === ""){
+        res.status(400).send();
+    }
     const tasks = users.find(user => user.id == userid).tasks;
     res.send({tasks: tasks});
 }
 
 exports.createTask = (req, res) => {
     const userid = req.params.userid;
+    const name = req.body.name;
+    console.log(typeof(name));
+    if(!name || name === ""){
+        res.status(400).send();
+    }
     const task = {id: uuid.v4(), name: req.body.name}
     const user = users.find(user => user.id == userid);
     user.tasks.push(task);
+    console.log(user);
     res.send(task);
+
 }
 
 exports.editTask = (req, res) => {
     const userid = req.params.userid;
     const user = users.find(user => user.id == userid);
     const task = user.tasks.find(task => task.id == req.params.id);
+    if (!req.body.name || req.body.name === "" || !task){
+        res.status(400).send();
+    }
     task.name = req.body.name;
     res.send(task);
+
 }
 
 exports.deleteTask = (req, res) => {
     const userid = req.params.userid;
     const user = users.find(user => user.id == userid);
+    const task = user.tasks.find(task => task.id == req.params.id);
+    if(!task){
+        res.status(400).send();
+    }
     user.tasks = user.tasks.filter(task => task.id != req.params.id);
+    console.log(users);
     res.status(204).send();
 }
