ca581d464e0015eaacaab240a4dff170190ee1c9
Cédric Chalhoub <cedric.chalhoub.1@ulaval.ca>
2022-12-20 16:15:44 -0500

    Remises finales

--- labo9/index.js
+++ labo9/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { createServer } = require("http");
+const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer, {
+const server = http.createServer(app);
+const io = new Server(server, {
     cors: {
         origin: "*",
         methods: ['GET', 'POST']
@@ -12,10 +12,18 @@ const io = new Server(httpServer, {
 });
 
 io.on("connection", (socket) => {
-    socket.on('chat_message', function(msg){
-        console.log("Message: " + msg);
+    console.log("A client connected");
+    socket.on("disconnect", (err) => {
+        console.log("user disconnected");
     });
+
+    socket.on('sendingMessage', (payload) => {
+        io.emit('broadCastMessageFromServer', payload);
+    })
 });
 
-httpServer.listen(8100);
-console.log("Server listening at http://localhost:8100");
+app.get('/', (req, res) => {res.sendFile(__dirname + '/views/index.html')});
+
+server.listen(8100, () => {
+    console.log("Server listening at http://localhost:8100");
+})
